0393065761535832701c52f284e7183e98ef3c11
seikikoga <seikikoga@gamania.com>
2014-01-15 12:22:35 +0900

    初回インポート

---
+++ vars.sh
@@ -0,0 +1,6 @@
+NAME=centos65-x86_64
+TYPE=RedHat_64
+INSTALLER="/Volumes/PD/PD/TDS/Archive/iso/CentOS-6.5-x86_64-minimal.iso"
+GUESTADDITIONS="/Applications/VirtualBox.app/Contents/MacOS/VBoxGuestAdditions.iso"
+HDD="${HOME}/VirtualBox VMs/${NAME}/${NAME}.vmdk"
+NATNET=10.0.2.0/24
